25a9345ecfd0064c2db2ea1bc9eafb15cb2da4bb
OllaWilk <alex.dev.wilk@gmail.com>
2025-02-18 20:54:16 +0100

    feat: add start button logic and timeout for challenge completion

--- src/components/TimerChallenge/TimerChallenge.jsx
+++ src/components/TimerChallenge/TimerChallenge.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TimerChallenge.css';
 
 export const TimerChallenge = ({ title, targetTime }) => {
+  const [timeStarted, setTimeStarted] = useState(false);
+  const [timeExpired, setTimeExpired] = useState(false);
+
+  const handleStart = () => {
+    setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+
+    setTimeStarted(true);
+  };
+
   return (
     <section className='challenge'>
       <h2>{title}</h2>
+      {timeExpired && <p>You lost !</p>}
       <p className='challenge-time'>
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timeStarted ? 'Stop' : 'Start'} Challenge
+        </button>
       </p>
-      <p>Time is running / Timer inactive</p>
+      <p className={timeStarted ? 'active' : undefined}>
+        Time is running / Timer inactive
+      </p>
     </section>
   );
 };
